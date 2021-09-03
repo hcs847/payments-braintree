87f178246e9eaa82addfa806f86fb74be00c54bb
Hila Caspi <caspi.home@gmail.com>
2021-09-02 22:43:16 -0700

    styling adjustments

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -6,7 +6,7 @@ function Landing() {
         <div className='flex flex-center'>
 
             <div className="card">
-                <p style={{ margin: '1rem' }}>Create an account or login</p>
+                <p style={{ margin: '1rem', fontWeight: '500' }}>Create an account or Login</p>
                 <button style={{ backgroundColor: 'red' }} className='btn'><a href='/auth/google'>Login with Google</a></button>
             </div>
 
